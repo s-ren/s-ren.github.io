5e0f11bdab027c4784b76bda8e5deaf18ff6e680
sr2262 <sr2262@cornell.edu>
2025-08-25 22:26:21 -0400

    add domain name and deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simple-portfolio.vicbox.dev/',
+    site: 'https://sileiren.com',
     integrations: [mdx(), react(), tailwind({
         applyBaseStyles: false,
     }), icon(), sitemap()],
