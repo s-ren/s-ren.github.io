974e30e03168d2d563919955c92c6e86cbc8119f
sr2262 <sr2262@cornell.edu>
2026-02-16 23:05:29 -0500

    update gitignore and clean up garbage

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_2myHVG from "./images/customizing-theme-color-schemes/cover.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fcustomizing-theme-color-schemes.md";
-import __ASTRO_IMAGE_IMPORT_1xIVfm from "./images/customizing-user-information/cover.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fcustomizing-user-information.mdx";
-export default new Map([["./images/customizing-theme-color-schemes/cover.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fcustomizing-theme-color-schemes.md", __ASTRO_IMAGE_IMPORT_2myHVG], ["./images/customizing-user-information/cover.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fcustomizing-user-information.mdx", __ASTRO_IMAGE_IMPORT_1xIVfm]]);
-		
